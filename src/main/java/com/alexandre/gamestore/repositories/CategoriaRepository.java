7a759e3533daa7e79cba77d46edf3b135510126e
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-17 14:13:42 -0300

    Camada service de Categoria implementada

--- src/main/java/com/alexandre/gamestore/repositories/CategoriaRepository.java
+++ src/main/java/com/alexandre/gamestore/repositories/CategoriaRepository.java
@@ -4,7 +4,10 @@ import com.alexandre.gamestore.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+    List<Categoria> findAllByNomeContainingIgnoreCase(String nome);
 }
--- src/main/java/com/alexandre/gamestore/services/CategoriaService.java
+++ src/main/java/com/alexandre/gamestore/services/CategoriaService.java
@@ -2,10 +2,12 @@ package com.alexandre.gamestore.services;
 
 import com.alexandre.gamestore.model.Categoria;
 import com.alexandre.gamestore.repositories.CategoriaRepository;
+import com.alexandre.gamestore.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -15,5 +17,32 @@ public class CategoriaService {
 
    public List<Categoria> findAll(){
        return categoriaRepository.findAll();
+   }
+
+   public Categoria findById(Long id){
+       Optional<Categoria> obj = categoriaRepository.findById(id);
+       return obj.orElseThrow(() -> new ObjectNotFoundException(
+               "Id " + id + " não encontrado " + Categoria.class.getName()));
+   }
+
+   public List<Categoria> findByName(String nome) {
+       return categoriaRepository.findAllByNomeContainingIgnoreCase(nome);
+   }
+
+   public Categoria insert(Categoria categoria){
+       categoria.setId(null);
+       categoria.setNome(categoria.getNome());
+       return  categoriaRepository.save(categoria);
+   }
+
+   public Categoria update(Categoria categoria){
+       categoriaRepository.findById(categoria.getId());
+       categoria.setNome(categoria.getNome());
+       return categoriaRepository.save(categoria);
+   }
+
+   public void delete(Long id){
+       categoriaRepository.findById(id);
+       categoriaRepository.deleteById(id);
    }
 }
