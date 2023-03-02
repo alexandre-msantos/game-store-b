42966da51feda4f121517dc6a728eabff722a4f9
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-03-02 16:39:41 -0300

    User controller class correction

--- src/main/java/com/alexandre/gamestore/controllers/UserController.java
+++ src/main/java/com/alexandre/gamestore/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.alexandre.gamestore.controllers;
 
 import com.alexandre.gamestore.model.User;
 import com.alexandre.gamestore.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping(value = "/{id}")
