1277162b87c250c33cad6c8d475e11dd4c6a9c99
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-03-02 08:39:19 -0300

    UserRepository implemented

---
+++ src/main/java/com/alexandre/gamestore/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alexandre.gamestore.repositories;
+
+import com.alexandre.gamestore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
