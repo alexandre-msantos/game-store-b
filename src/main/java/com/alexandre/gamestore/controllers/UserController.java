bd95b82112cfbbd0cf7f0620fb6584059f644df3
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-03-07 17:42:22 -0300

    Added findAll method

--- src/main/java/com/alexandre/gamestore/controllers/UserController.java
+++ src/main/java/com/alexandre/gamestore/controllers/UserController.java
@@ -1,26 +1,35 @@
 package com.alexandre.gamestore.controllers;
 
 import com.alexandre.gamestore.model.User;
-import com.alexandre.gamestore.repositories.UserRepository;
+import com.alexandre.gamestore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService service;
 
-    @GetMapping(value = "/{id}")
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.insertUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    /*@GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
-        return userRepository.findById(id)
+        return service.findById(id)
                 .map(obj -> ResponseEntity.ok().body(obj))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
+    }*/
 }
