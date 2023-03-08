7b678ba88c92e6ee3693a4408ac6dd95c753e695
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-03-08 13:47:15 -0300

    Added exception handling

--- src/main/java/com/alexandre/gamestore/services/impl/UserServiceImpl.java
+++ src/main/java/com/alexandre/gamestore/services/impl/UserServiceImpl.java
@@ -1,24 +1,36 @@
 package com.alexandre.gamestore.services.impl;
 
 import com.alexandre.gamestore.model.User;
+import com.alexandre.gamestore.repositories.UserRepository;
 import com.alexandre.gamestore.services.UserService;
+import com.alexandre.gamestore.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repo;
+
     @Override
     public User insertUser(User user) {
-        return null;
+        return repo.save(user);
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        return repo.findAll();
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado: " + id + " Tipo: " + User.class.getName()));
     }
 
     @Override
