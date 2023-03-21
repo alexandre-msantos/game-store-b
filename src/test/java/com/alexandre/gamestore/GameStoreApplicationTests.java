417abdcab66a46e638a1f2849c946269802956a6
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-03-21 08:12:43 -0300

    Test for main class

--- src/test/java/com/alexandre/gamestore/GameStoreApplicationTests.java
+++ src/test/java/com/alexandre/gamestore/GameStoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GameStoreApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        GameStoreApplication.main(new String[]{});
     }
-
 }
